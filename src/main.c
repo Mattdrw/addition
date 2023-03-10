b234f22dc40438cb349f88d23c0e6835401779b0
Mattdrw <matteo.darwich@gmail.com>
2023-03-10 14:47:48 +0100

    Commentaire de komo

--- src/main.c
+++ src/main.c
@@ -7,6 +7,6 @@ int main(int argc, char const *argv[]) {
   int a = 10, b = 5;
 
   printf("%d + %d = %d \n",a, b, somme(a, b) ); // ajout d'un commentaire <== ICI 
-  printf("%d - %d = %d \n",a, b, soustraction(a, b));
+  printf("%d - %d = %d \n",a, b, soustraction(a, b)); // komo
   return 0;
 }
